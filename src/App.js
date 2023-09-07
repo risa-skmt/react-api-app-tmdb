84bb2c6ad9a11fe6c93c2168a9aaf86b1f589562
risa-skmt <skmt.risa@gmail.com>
2023-09-08 00:25:50 +0900

    title search (input空にできていない)

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-// import ImageList from "@mui/material/ImageList";
-// import ImageListItem from "@mui/material/ImageListItem";
-// import ImageListItemBar from "@mui/material/ImageListItemBar";
-
-function App() {
+const App = () => {
+  const [originalMovies, setOriginalMovies] = useState([]); //全now playing
   const [movies, setMovies] = useState([]);
 
-  async function callAPI() {
-    const res = await fetch(
+  useEffect(() => {
+    fetch(
       "https://api.themoviedb.org/3/movie/now_playing?api_key=f4d043da0bfd25667e6825c40156d398"
-    );
-    const data = await res.json();
-    //console.log(data);
-    setMovies(data.results);
-  }
-  callAPI();
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+        setOriginalMovies(data.results);
+      });
+  }, []);
 
+  // async function callAPI() {
+  //   const res = await fetch(
+  //     "https://api.themoviedb.org/3/movie/now_playing?api_key=f4d043da0bfd25667e6825c40156d398"
+  //   );
+  //   const data = await res.json();
+  //   setMovies(data.results);
+  // }
+  // callAPI();
+
+  const [searchWord, setSearchWord] = useState("");
+  const search = () => {
+    const filteredMovies = originalMovies.filter((movie) => {
+      return movie.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    setMovies(filteredMovies);
+  };
   return (
     <div>
       <h2>Now Plaing in the theater</h2>
+      <input onChange={(e) => setSearchWord(e.target.value)} />
+      <button onClick={search}>search</button>
       <div className="contents">
-        {/* <ImageList sx={{ width: 900 }} cols={3} gap={11}>
-        {movies.map((movie) => (
-          <ImageListItem key={movie.poster}>
-            <img
-              src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <ImageListItemBar title={movie.title} position="below" />
-          </ImageListItem>
-        ))}
-      </ImageList> */}
-
         {movies.map((movie) => (
           <div className="content">
             <img
@@ -48,6 +52,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
