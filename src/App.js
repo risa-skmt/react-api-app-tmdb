1771f84a71d23d3c22fd7714399300dd0d9f66b4
risa-skmt <skmt.risa@gmail.com>
2023-09-08 23:15:24 +0900

    Route設定

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
 // import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NowPlaying from "./components/NowPlaying.js";
+import ScreenTime from "./components/ScreenTime.js";
 
 const App = () => {
-  const [originalMovies, setOriginalMovies] = useState([]); //全now playing
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=f4d043da0bfd25667e6825c40156d398"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-        setOriginalMovies(data.results);
-      });
-  }, []);
-
-  // async function callAPI() {
-  //   const res = await fetch(
-  //     "https://api.themoviedb.org/3/movie/now_playing?api_key=f4d043da0bfd25667e6825c40156d398"
-  //   );
-  //   const data = await res.json();
-  //   setMovies(data.results);
-  // }
-  // callAPI();
-
-  const [searchWord, setSearchWord] = useState("");
-  const search = (e) => {
-    e.preventDefault();
-    const filteredMovies = originalMovies.filter((movie) => {
-      return movie.title.toLowerCase().includes(searchWord.toLowerCase());
-    });
-    setMovies(filteredMovies);
-    setSearchWord("");
-  };
   return (
     <div>
-      <h2>Now Plaing in the theater</h2>
-      <form onSubmit={search}>
-        <input
-          value={searchWord}
-          onChange={(e) => setSearchWord(e.target.value)}
-        />
-        <button>search</button>
-      </form>
-      <div className="contents">
-        {movies.map((movie) => (
-          <div className="content">
-            <img
-              src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
-              alt={movie.title}
-            ></img>
-            <p key={movie.name}>{movie.title}</p>
-            {/* <li key={movie.overview}>{movie.overview}</li> */}
-          </div>
-        ))}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<NowPlaying />} />
+          <Route path="/screen_time" element={<ScreenTime />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
