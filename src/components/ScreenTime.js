d649b3b4d5b4c58a1b418305d1bf87a558711667
risa-skmt <skmt.risa@gmail.com>
2023-11-22 09:17:10 +0900

    時間option追加

--- src/components/ScreenTime.js
+++ src/components/ScreenTime.js
@@ -136,18 +136,18 @@ const ScreenTime = () => {
         value={selectedHour}
         onChange={(e) => setSelectedHour(e.target.value)}
       >
-        <option value="9">21</option>
-        <option value="10">21</option>
-        <option value="11">21</option>
-        <option value="12">21</option>
-        <option value="13">21</option>
-        <option value="14">21</option>
-        <option value="15">21</option>
-        <option value="16">21</option>
-        <option value="17">21</option>
-        <option value="18">21</option>
-        <option value="19">21</option>
-        <option value="20">21</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
         <option value="21">21</option>
         <option value="22">22</option>
         <option value="23">23</option>
